e819911c712891f31c862c6061c575c8bb2a4ee1
Matt Lilley <m.k.lilley@gmail.com>
2025-11-30 10:14:57 +0000

    Adds main activity

---
+++ app/src/main/java/com/example/climatemonitor/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.climatemonitor
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // No UI needed — empty Activity is fine
+    }
+}
